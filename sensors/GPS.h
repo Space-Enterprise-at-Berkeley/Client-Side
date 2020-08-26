3c088fdcb285c12091efdf62de69da23ac42863b
Vamshi Balanaga <vamshi.balanaga@gmail.com>
2020-08-25 18:21:39 -0700

    wip: convert sensor libs to standardized format

---
+++ sensors/GPS.h
@@ -0,0 +1,26 @@
+/*
+  GPS.h - A c++ library to interface with the Arduino Ultimate GPS MTK3339.
+  Created by Vamshi Balanaga, Aug 21, 2020.
+*/
+
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+
+class GPS {
+  public:
+    GPS(HardwareSerial *ser); // Constructor when using Serial
+    GPS(TwoWire *theWire);    // Constructor when using I2C
+    GPS(SPIClass *theSPI, int8_t cspin); // Constructor when using SPI
+    read_data();
+  private:
+    uint8_t commMethod; // 1 for HardwareSerial, 2 for SoftwareSerial, 3 for I2c, 4 for SPI
+
+
+}
+
+GPS::GPS(HardwareSerial *ser){
+
+}
+
+Adafruit_GPS GPS(&GPSSerial);
