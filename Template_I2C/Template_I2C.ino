61f4b73350066893038fb867c19fbcc7ef46804f
Vainavi Viswanath <vainaviv2001@berkeley.edu>
2020-09-20 21:32:31 -0700

    Added comments to top of file and functions

--- Template_I2C/Template_I2C.ino
+++ Template_I2C/Template_I2C.ino
@@ -1,14 +1,21 @@
+/*
+ * Template_I2C.ino - A c++ program that uses I2C communication to receive requests
+ * for data from Brain_I2C.ino and send the corresponding data back.
+ * Created by Vainavi Viswanath, Aug 21, 2020.
+ */
 #include <Wire.h>
 
+/*
+ * Change this address according to the board address this code is uploaded on
+ */
 int Address = 8;
-int sensorNum = 0;
 
-//function pointer
+int sensorNum = 0;
 void (*generalFunc)(float*);
 
 void setup() {
-  Wire.begin(Address);           // join i2c bus with address #8
-  Wire.onRequest(requestEvent); // register event
+  Wire.begin(Address);    
+  Wire.onRequest(requestEvent); 
   Wire.onReceive(updateSensorNum);
   Serial.begin(9600);
 }
@@ -17,11 +24,18 @@ void loop() {
   delay(100);
 }
 
+/*
+ * Data structure to allow the conversion of bytes to floats and vice versa.
+ */
 union floatArrToBytes {
   char buffer[24];
   float sensorReadings[6];
 } farrbconvert;
 
+/*
+ * When a request comes from the brain, collects data from the requested sensor
+ * and sends back float array with data points. 
+ */
 void requestEvent() {
   float data[6] = {0,0,0,0,0,0};
   generalFunc(data);
@@ -31,6 +45,9 @@ void requestEvent() {
   Wire.write(farrbconvert.buffer, 24);
 }
 
+/*
+ * Selects the corresponding function to the sensor which data is requested from.
+ */
 void updateSensorNum(int howMany) {
   while (!Wire.available());
   sensorNum = Wire.read();
@@ -41,6 +58,9 @@ void updateSensorNum(int howMany) {
   }
 }
 
+/*
+ * Method to collect data from ______
+ */
 void getReading0(float *data) {
   //get the sensor reading here
   data[0] = 36.89;
@@ -48,6 +68,9 @@ void getReading0(float *data) {
   data[2] = 30.45;
 }
 
+/*
+ * Method to collect data from ______
+ */
 float getReading1(float *data) {
   //get the sensor reading here
   data[0] = 14.26;
