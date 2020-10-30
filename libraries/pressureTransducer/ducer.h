6a973199296f63d647a302e3efa4a0872795153f
Vamshi Balanaga <vamshi.balanaga@gmail.com>
2020-10-29 23:35:21 -0700

    lib

--- libraries/pressureTransducer/ducer.h
+++ libraries/pressureTransducer/ducer.h
@@ -8,14 +8,13 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-// #include "ADS1219.h"
+#include "ADS1219.h"
 // #include <TwoWire.h>
 
 using namespace std;
 
 namespace Ducers {
 
-  #define CONFIG_REGISTER_ADDRESS 0x40
 
   #define AIN0 0x61
   #define AIN1 0x81
@@ -31,33 +30,33 @@ namespace Ducers {
   uint8_t ADC1_ADDR = 0b1001010;
   uint8_t ADC2_ADDR = 0b1001000;
 
-  long getData(uint8_t address, uint8_t conf) {
-    Wire.beginTransmission(address);
-    Wire.write(CONFIG_REGISTER_ADDRESS);
-    Wire.write(conf);
-    Wire.endTransmission();
-
-    Wire.beginTransmission(address);
-    Wire.write(0x08);
-    Wire.endTransmission();
-
-    while(digitalRead(DTRDY_PIN_1)==1);
-    delay(1);
-    Wire.beginTransmission(address);
-    Wire.write(0x10);
-    Wire.endTransmission();
-
-    Wire.requestFrom((uint8_t)address,(uint8_t)3);
-    long data32 = Wire.read();
-    data32 <<= 8;
-    data32 |= Wire.read();
-    data32 <<= 8;
-    data32 |= Wire.read();
-    return (data32 << 8) >> 8;
-  }
+  // long getData(uint8_t address, uint8_t conf) {
+  //   Wire.beginTransmission(address);
+  //   Wire.write(CONFIG_REGISTER_ADDRESS);
+  //   Wire.write(conf);
+  //   Wire.endTransmission();
   //
-  // ADS1219 ads1(DTRDY_PIN_1, ADC1_ADDR);
-  // ADS1219 ads2(DTRDY_PIN_2, ADC2_ADDR);
+  //   Wire.beginTransmission(address);
+  //   Wire.write(0x08);
+  //   Wire.endTransmission();
+  //
+  //   while(digitalRead(DTRDY_PIN_1)==1);
+  //   delay(1);
+  //   Wire.beginTransmission(address);
+  //   Wire.write(0x10);
+  //   Wire.endTransmission();
+  //
+  //   Wire.requestFrom((uint8_t)address,(uint8_t)3);
+  //   long data32 = Wire.read();
+  //   data32 <<= 8;
+  //   data32 |= Wire.read();
+  //   data32 <<= 8;
+  //   data32 |= Wire.read();
+  //   return (data32 << 8) >> 8;
+  // }
+
+  ADS1219 ads1(DTRDY_PIN_1, ADC1_ADDR);
+  ADS1219 ads2(DTRDY_PIN_2, ADC2_ADDR);
 
   // first 3 bytes are mux select, gain, data rate, data rate, conversion mode, v ref
   TwoWire *localWire;
@@ -106,23 +105,23 @@ namespace Ducers {
     // resetADC(ADC1_ADDR);
     // resetADC(ADC2_ADDR);
 
-    // ads1.begin();
-    // ads2.begin();
-    //
-    // ads1.setConversionMode(SINGLE_SHOT);
-    // ads2.setConversionMode(SINGLE_SHOT);
-    //
-    // ads1.start();
-    // ads2.start();
-    //
-    // ads1.setVoltageReference(REF_EXTERNAL);
-    // ads2.setVoltageReference(REF_EXTERNAL);
-    //
-    // ads1.setGain(GAIN_ONE);
-    // ads2.setGain(GAIN_ONE);
-    //
-    // ads1.setDataRate(20);
-    // ads2.setDataRate(20);
+    ads1.begin();
+    ads2.begin();
+
+    ads1.setConversionMode(SINGLE_SHOT);
+    ads2.setConversionMode(SINGLE_SHOT);
+
+    ads1.start();
+    ads2.start();
+
+    ads1.setVoltageReference(REF_EXTERNAL);
+    ads2.setVoltageReference(REF_EXTERNAL);
+
+    ads1.setGain(GAIN_ONE);
+    ads2.setGain(GAIN_ONE);
+
+    ads1.setDataRate(20);
+    ads2.setDataRate(20);
 
     // calibration1 = calibrateADC(ADC1_ADDR);
     // calibration2 = calibrateADC(ADC2_ADDR);
@@ -160,14 +159,14 @@ namespace Ducers {
   void readPropaneTankPressure(float *data) {
     //AIN0
     // data[0] = float(ads1.getData(AIN0) - calibration1);
-    data[0] = getData(ADC1_ADDR, AIN0);
+    data[0] = ads1.readData(AIN0);
     data[1] = -1;
   }
 
   void readLOXTankPressure(float *data) {
     // AIN1
     // data[0] = float(ads1.readData(AIN1) - calibration1);
-    data[0] = getData(ADC1_ADDR, AIN1);
+    data[0] = ads1.readData(AIN1);
 
     data[1] = -1;
   }
@@ -175,7 +174,7 @@ namespace Ducers {
   void readPropaneInjectorPressure(float *data) {
     // AIN2
     // data[0] = float(ads1.readData(AIN2) - calibration1);
-    data[0] = getData(ADC1_ADDR, AIN2);
+    data[0] = ads1.readData(AIN2);
 
     data[1] = -1;
   }
@@ -183,28 +182,28 @@ namespace Ducers {
   void readLOXInjectorPressure(float *data) {
     //AIN3
     // data[0] = float(ads1.readData(AIN3) - calibration1);
-    data[0] = getData(ADC1_ADDR, AIN3);
+    data[0] = ads1.readData(AIN3);
     data[1] = -1;
   }
 
   void readPressurantTankPressure(float *data) {
     //AIN 0 on ADC 2
     // data[0] = float(ads2.readData(AIN0) - calibration2);
-    data[0] = getData(ADC2_ADDR, AIN0);
+    data[0] = ads2.readData(AIN0);
     data[1] = -1;
   }
 
   void readAllLowPressures(float *data) {
-    data[0] = float(getData(ADC1_ADDR, AIN1) - calibration1);
-    data[1] = float(getData(ADC1_ADDR, AIN0) - calibration1);
-    data[2] = float(getData(ADC1_ADDR, AIN3) - calibration1);
-    data[3] = float(getData(ADC1_ADDR, AIN2) - calibration1);
+    // data[0] = float(getData(ADC1_ADDR, AIN1) - calibration1);
+    // data[1] = float(getData(ADC1_ADDR, AIN0) - calibration1);
+    // data[2] = float(getData(ADC1_ADDR, AIN3) - calibration1);
+    // data[3] = float(getData(ADC1_ADDR, AIN2) - calibration1);
     data[4] = -1;
   }
 
   void readAllPressures(float *data) {
     readAllLowPressures(data);
-    data[4] = float(getData(ADC2_ADDR, AIN0) - calibration2);
+    // data[4] = float(getData(ADC2_ADDR, AIN0) - calibration2);
     data[5] = -1;
   }
 }
