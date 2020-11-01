a568926236878d046a946521af622e1daa253ee7
Vamshi Balanaga <vamshi.balanaga@gmail.com>
2020-11-01 09:26:00 -0800

    stable for waterflow

--- libraries/batteryMonitor/batteryMonitor.h
+++ libraries/batteryMonitor/batteryMonitor.h
@@ -44,7 +44,7 @@ namespace batteryMonitor {
     readPowerConsumption(data);
     data[1] = data[0];
     data[0] = ina.readBusVoltage();
-    data[2] = energyConsumed;
+    data[2] = -1*ina.readShuntCurrent(); //wiring is flipped on pcb
     data[3] = -1;
   }
 
